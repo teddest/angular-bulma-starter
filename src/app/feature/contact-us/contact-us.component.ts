21cc84a01306e27b7dd719aa978ee9c2882bfbd0
Tedd Van Diest <tedd@Tedds-MacBook-Pro.local>
2019-05-27 01:20:09 -0400

    chore(lint): Fix lint warnings

--- src/app/feature/contact-us/contact-us.component.ts
+++ src/app/feature/contact-us/contact-us.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-contact-us',
+  selector: 'abs-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
