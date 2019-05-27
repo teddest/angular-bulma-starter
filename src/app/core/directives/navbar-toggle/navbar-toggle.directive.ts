5dc1c8d448cefdb09ba27354049ddfd8ad4084aa
Tedd Van Diest <tedd@Tedds-MacBook-Pro.local>
2019-05-27 01:14:22 -0400

    chore: Refactor navbar toggle directive

--- src/app/core/directives/navbar-toggle/navbar-toggle.directive.ts
+++ src/app/core/directives/navbar-toggle/navbar-toggle.directive.ts
@@ -5,23 +5,23 @@ import { DOCUMENT } from '@angular/common';
   selector: '.burger,a.navbar-item',
 })
 export class NavbarToggleDirective {
-  constructor(@Inject(DOCUMENT) private doc: any) { }
+  constructor(@Inject(DOCUMENT) private document: any) { }
 
   @HostListener('click', ['$event'])
   onClick(e: any) {
-    const $burgers = this.doc.querySelectorAll('.burger');
+    const burgers = this.document.querySelectorAll('.burger');
 
-    if ($burgers.length > 0) {
-      $burgers.forEach(($el: any) => {
-        const $target = this.doc.getElementById($el.dataset.target);
+    if (burgers.length > 0) {
+      burgers.forEach((el: any) => {
+        const dataTarget = this.document.getElementById(el.dataset.target);
+        const isDataTargetActive = dataTarget.classList.contains('is-active');
+        const isNavbarItem = e.target.className === 'navbar-item';
 
-        // Don't toggle is-active class if $target isn't active when clicking navbar links
-        if (!$target.classList.contains('is-active') && e.target.className === 'navbar-item') {
-          return;
-        }
+        // Just return if dataTarget isn't active when clicking navbar links
+        if ([!isDataTargetActive, isNavbarItem].every(Boolean)) { return; }
 
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
+        el.classList.toggle('is-active');
+        dataTarget.classList.toggle('is-active');
       });
     }
   }
